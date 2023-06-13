48a21ba89a183dede4c0057b1f2400aed328bf71
C Ding <cding@cs.rochester.edu>
2023-06-13 11:48:51 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,31 @@
-use v3pluss::loop_tree::LoopTNode;
+use dace::ast::{Node, Stmt};
 use std::rc::Rc;
 
-pub fn matmul(n: usize) -> Rc<LoopTNode> {
+pub fn matmul(n: usize) -> Rc<Node> {
     // n: usize is array dim
     let ubound = n as i32; // loop bound
     // creating C[i,j] += A[i,k] * B[k,j]
-    let s_ref_c = LoopTNode::new_ref("C", vec![n, n], |ijk| {
+    let s_ref_c = Node::new_ref("C", vec![n, n], |ijk| {
         vec![ijk[0] as usize, ijk[1] as usize]
     });
-    let s_ref_a = LoopTNode::new_ref("A", vec![n, n], |ijk| {
+    let s_ref_a = Node::new_ref("A", vec![n, n], |ijk| {
         vec![ijk[0] as usize, ijk[2] as usize]
     });
-    let s_ref_b = LoopTNode::new_ref("B", vec![n, n], |ijk| {
+    let s_ref_b = Node::new_ref("B", vec![n, n], |ijk| {
         vec![ijk[2] as usize, ijk[1] as usize]
     });
 
     // creating loop k = 0, n { s_ref }
-    let k_loop_ref = LoopTNode::new_single_loop("k", 0, ubound);
-    LoopTNode::extend_loop_body(&k_loop_ref, &s_ref_c);
-    LoopTNode::extend_loop_body(&k_loop_ref, &s_ref_a);
-    LoopTNode::extend_loop_body(&k_loop_ref, &s_ref_b);
+    let k_loop_ref = Node::new_single_loop("k", 0, ubound);
+    Node::extend_loop_body(&k_loop_ref, &s_ref_c);
+    Node::extend_loop_body(&k_loop_ref, &s_ref_a);
+    Node::extend_loop_body(&k_loop_ref, &s_ref_b);
     // creating loop j = 0, n
-    let j_loop_ref = LoopTNode::new_single_loop("j", 0, ubound);
-    LoopTNode::extend_loop_body(&j_loop_ref, &k_loop_ref);
+    let j_loop_ref = Node::new_single_loop("j", 0, ubound);
+    Node::extend_loop_body(&j_loop_ref, &k_loop_ref);
     // creating loop i = 0, n
-    let i_loop_ref = LoopTNode::new_single_loop("i", 0, ubound);
-    LoopTNode::extend_loop_body(&i_loop_ref, &j_loop_ref);
+    let i_loop_ref = Node::new_single_loop("i", 0, ubound);
+    Node::extend_loop_body(&i_loop_ref, &j_loop_ref);
 
     i_loop_ref
 }
